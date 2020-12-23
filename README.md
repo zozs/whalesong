c63896a7d4c0ed0eba4866cf39a8d1f3e768edfd
Linus Karlsson <linus@zozs.se>
2020-12-23 17:14:09 +0100

    Add code for PoC that seems to work.

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
 # whalesong
+
 Distributed container registry built on hypercores and IPFS
+
+## About
+
+## Installation
+
+Requirements:
+
+* Node 14 or later
+
+Clone this repository, and install dependencies.
+
+```
+git clone https://github.com/zozs/whalesong
+cd whalesong
+npm install
+```
+
+## Usage
+
+Launch the daemon by running
+
+`node index.js`
+
+inside the directory above. When the deamon is up, you now have a container registry listening on port 5005. You can then try to push and pull to the distributed registry.
+
+### Example
+
+After launching the image above, try to pull and run the following test image, which will print a small greeting on your screen.
+
+```
+$ docker run 127.0.0.1:5005/<pubkey>/hello:latest
+A distributed greeting to you, my friend :)
+```
+
+### Pull an image
+
+A container image can be pulled using docker with
+
+`docker pull <url of image>`
+
+where the url will be of the following form for whalesong:
+
+`127.0.0.1:5005/f99e669050e4e3f1e8a2f8f86238080b48e3d6e432dd3bd83c47d2aed09a3114/hello:latest`
+
+The url consist of the following parts:
+
+* `127.0.0.1:5005`: The url of the registry that is running on your local machine. You will always use this url for both pulling and pushing to the distributed registry.
+* `f99e669050e4e3f1e8a2f8f86238080b48e3d6e432dd3bd83c47d2aed09a3114`: A long hexadecimal string, which is the encoded public key of the organisation/person whose image you want to pull or push. You can only push to public keys you own yourself, but can pull from any public key you know of.
+* `hello:latest`: The image name (`hello`) and tag (`latest`) for the given image you want to pull. An organization or person may have multiple images and tags.
+
+### Push an image
+
+A container image can be pushed using docker with
+
+`docker push <url of image>`
+
+where the url must contain the url of the localhost registry,as well as your own public key, and the name you want to give your image. Your own public key is printed when you launch the daemon.
+
+For example, assuming you have the public key `abcd1234`, and a Dockerfile in the current directory, build and push the image using:
+
+```
+docker build -t 127.0.0.1:5005/abcd1234/myownimage:latest .
+docker push 127.0.0.1:5005/abcd1234/myownimage:latest
+```
+
+Anyone else running `whalesong` can now pull your image using the url `127.0.0.1:5005/abcd1234/myownimage:latest` and run it on their own computer.
+
+### Other facts
+
+The data is stored in the `~/.whalesong` directory in your home.
+
+## Todo
+
+* Make container registry pass conformance tests (see below).
+* Increase performance and reduce memory usage by not caching layers in memory when passing them between IPFS and the registry.
+* Support more registry operations, such as content discovery and content management.
+* Support multiple writable feeds from a single host.
+* General reliability improvements.
+
+## Conformance
+
+The current implementation of the container registry does _not_ fully conform to the [Open Container Initiative Distribution Specification](https://github.com/opencontainers/distribution-spec/blob/master/spec.md). It seems to work well enough with Docker anyway, but it would be nice to make the conformance tests pass.
+
+## License
+
+Whalesong is licensed under GNU AGPL v3 or later, see the `LICENSE` file for the full license.
+
+```
+whalesong - distributed container registry
+Copyright (c) 2020, Linus Karlsson
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+```
---
+++ index.js
@@ -0,0 +1,129 @@
+import express from 'express'
+import DistributedStorage from './lib/distributed-storage.js'
+
+const app = express()
+
+const host = process.env.WHALESONG_HOST || 'localhost'
+const port = process.env.WHALESONG_PORT || '5005'
+const hostport = `http://${host}:${port}`
+
+async function setUpApp () {
+  console.debug('Initializing storage provider')
+  const storage = new DistributedStorage()
+  await storage.init()
+
+  app.use(express.raw({ type: () => true, limit: 1024 * 1024 * 1024 })) // max 1 GiB chunk
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.get('/v2/', (req, res) => {
+    res.send('{}')
+  })
+
+  app.post('/v2/:org/:name/blobs/uploads/', async (req, res) => {
+    const { org, name } = req.params
+    const uuid = await storage.newUpload(org, name)
+
+    console.debug('Creating temporary upload')
+
+    res.set('Location', `${hostport}/v2/${org}/${name}/blobs/uploads/${uuid}`)
+    res.set('Range', '0-0')
+    res.sendStatus(202)
+  })
+
+  app.patch('/v2/:org/:name/blobs/uploads/:uuid', async (req, res) => {
+    const { org, name, uuid } = req.params
+    const uploaded = await storage.patchUpload(org, name, uuid, req.body)
+
+    console.debug(`UUID ${uuid} now has ${uploaded} bytes.`)
+    res.set('Location', `${hostport}/v2/${org}/${name}/blobs/uploads/${uuid}`)
+    res.set('Range', `0-${uploaded}`)
+    res.sendStatus(202)
+  })
+
+  app.put('/v2/:org/:name/blobs/uploads/:uuid', async (req, res) => {
+    const { org, name, uuid } = req.params
+    const expectedDigest = req.query.digest
+    const { digest, uploaded } = await storage.putUpload(org, name, uuid, req.body)
+
+    console.debug(`UUID ${uuid} now has ${uploaded} bytes after finish.`)
+    if (expectedDigest !== digest) {
+      console.debug(`expected digest ${expectedDigest} did not match actual digest ${digest}`)
+      res.sendStatus(400)
+    } else {
+      res.set('Docker-Content-Digest', digest)
+      res.set('Location', `${hostport}/v2/${org}/${name}/blobs/${digest}`)
+      res.sendStatus(204)
+    }
+  })
+
+  app.head('/v2/:org/:name/blobs/:digest', async (req, res) => {
+    const { org, name, digest } = req.params
+    if (await storage.hasBlob(org, name, digest)) {
+      console.debug(`Blob with digest ${digest} exists.`)
+      res.set('Docker-Content-Digest', digest)
+      res.sendStatus(200)
+    } else {
+      console.debug(`Blob with digest ${digest} does not exists.`)
+      res.sendStatus(404)
+    }
+  })
+
+  app.get('/v2/:org/:name/blobs/:digest', async (req, res) => {
+    const { org, name, digest } = req.params
+    const data = await storage.getBlob(org, name, digest)
+    if (data !== null) {
+      console.debug(`Retrieving blob with digest ${digest}.`)
+      res.set('Docker-Content-Digest', digest)
+      res.send(data)
+    } else {
+      console.debug(`Blob with digest ${digest} does not exists.`)
+      res.sendStatus(404)
+    }
+  })
+
+  app.head('/v2/:org/:name/manifests/:tag', async (req, res) => {
+    const { org, name, tag } = req.params
+    const { digest } = await storage.getManifest(org, name, tag)
+    if (digest !== null) {
+      res.set('Docker-Content-Digest', digest)
+      res.sendStatus(200)
+    } else {
+      console.debug(`Manifest with tag ${tag} does not exist`)
+      res.sendStatus(404)
+    }
+  })
+
+  app.get('/v2/:org/:name/manifests/:tag', async (req, res) => {
+    const { org, name, tag } = req.params
+    const { digest, data } = await storage.getManifest(org, name, tag)
+    if (digest !== null) {
+      console.debug(`Retrieving manifest with digest ${digest}`)
+      res.set('Docker-Content-Digest', digest)
+      res.set('Content-Type', 'application/vnd.docker.distribution.manifest.v2+json')
+      res.send(data)
+    } else {
+      console.debug(`Manifest with tag ${tag} does not exist`)
+      res.sendStatus(404)
+    }
+  })
+
+  app.put('/v2/:org/:name/manifests/:tag', async (req, res) => {
+    const { org, name, tag } = req.params
+    const digest = await storage.putManifest(org, name, tag, req.body)
+
+    console.debug(`Stored manifest ${org}/${name}:${tag} with digest ${digest}`)
+
+    res.set('Docker-Content-Digest', digest)
+    res.set('Location', `${hostport}/v2/${org}/${name}/manifests/${digest}`)
+    res.sendStatus(201)
+  })
+
+  app.listen(port, () => {
+    console.log(`Node-registry listening at ${hostport}`)
+  })
+}
+
+setUpApp().catch(e => console.error(`top-level exception: ${e} json: ${JSON.stringify(e)}`))
