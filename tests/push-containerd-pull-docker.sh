9706de9aef8ecc5820f20a8018aba97c05212531
Linus Karlsson <linus@zozs.se>
2021-03-20 15:45:12 +0100

    Add type to data structure list of blob locations. Test multiarch pushs.

--- tests/push-containerd-pull-docker.sh
+++ tests/push-containerd-pull-docker.sh
@@ -18,9 +18,32 @@ docker pull "$WHALESONG_IMAGE"
 OUTPUT=$(docker run --rm "$WHALESONG_IMAGE" python -c 'print("hi from ci")')
 
 if [[ $OUTPUT == "hi from ci" ]]; then
-	echo "Pull/push test successful!"
+	echo "Pull/push test successful for single platform!"
 else
-	echo "Pull/push test failed!"
+	echo "Pull/push test failed for single platform!"
+	exit 1
+fi
+
+# Repeat test but push _all_ platforms, i.e., test creating an index (manifest list) too.
+ctr image rm "$FULL_TEST_IMAGE" "$WHALESONG_IMAGE" || true
+docker rmi "$WHALESONG_IMAGE" || true
+
+ctr image pull --all-platforms "$FULL_TEST_IMAGE"
+ctr image tag "$FULL_TEST_IMAGE" "$WHALESONG_IMAGE"
+
+# Push all platforms with containerd.
+ctr image push --plain-http "$WHALESONG_IMAGE"
+
+# Pull using docker
+docker pull "$WHALESONG_IMAGE"
+
+# Run image using docker
+OUTPUT=$(docker run --rm "$WHALESONG_IMAGE" python -c 'print("hi from ci")')
+
+if [[ $OUTPUT == "hi from ci" ]]; then
+	echo "Pull/push test successful for multiple platforms!"
+else
+	echo "Pull/push test failed for multiple platforms!"
 	exit 1
 fi
 
@@ -28,4 +51,3 @@ fi
 set +e
 ctr image rm "$FULL_TEST_IMAGE" "$WHALESONG_IMAGE" || true
 docker rmi "$WHALESONG_IMAGE" || true
-
